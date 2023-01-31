84b5cb1dd50e6b89cc2c18655d7c931c44f802ff
Hamza Ait Addi <123383990+HamzaEng@users.noreply.github.com>
2023-01-31 20:09:31 +0100

    Update db.class.php

--- App/Classes/db.class.php
+++ App/Classes/db.class.php
@@ -1,27 +1,20 @@
 <?php
 
-class Db{
-    private $server;
-    private $db;
-    private $user;
-    private $password;
-    public function __construct()
+class Db
+{
+    const server = "localhost";
+    const db = "BTS";
+    const user = "root";
+    const password = "";
+    static public function connect()
     {
-        $this->server = "localhost";
-        $this->db = "BTS";
-        $this->user = "root";
-        $this->password = "";
-    }
-    static public function connect(){
         try {
-            $database = new Db();
-            $pdo = new PDO("mysql:host=".$database->server.";dbname=".$database->db,$database->user,$database->password);
+            $pdo = new PDO("mysql:host=" . self::server . ";dbname=" . self::db, self::user, self::password);
             $pdo->exec("set names utf8");
             return $pdo;
-        }catch(PDOException $error) {
-            die("Error ".$error->getMessage());
+        } catch (PDOException $error) {
+            die("Error " . $error->getMessage());
         }
-
     }
-}
+} 
  
